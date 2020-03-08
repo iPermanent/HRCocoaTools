f826ad95c870270f4a76e1e739d8db1aba63cd67
zhangheng <henryzhangios@gmail.com>
2020-03-08 16:40:15 +0800

    #更新设备类型

--- HRCocoaTools/Classes/Util/HRDevice.h
+++ HRCocoaTools/Classes/Util/HRDevice.h
@@ -9,35 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger, HRDeviceType){
-    HRDeviceTypeiPhone  =   0,
-    HRDeviceTypeiPhone3G,
-    HRDeviceTypeiPhone3GS,
-    HRDeviceTypeiPhone4,
-    HRDeviceTypeiPhone4S,
-    HRDeviceTypeiPhone5,
-    HRDeviceTypeiPhone5c,
-    HRDeviceTypeiPhone5S,
-    HRDeviceTypeiPhone6,
-    HRDeviceTypeiPhone6Plus,
-    HRDeviceTypeiPhone6s,
-    HRDeviceTypeiPhone6sPlus,
-    HRDeviceTypeiPad1,
-    HRDeviceTypeiPad2,
-    HRDeviceTypeiPad3,
-    HRDeviceTypeiPad4,
-    HRDeviceTypeiPadAir,
-    HRDeviceTypeiPadAir2,
-    HRDeviceTypeiPadMini,
-    HRDeviceTypeiPadMini2,
-    HRDeviceTypeiPadMini3,
-    HRDeviceTypeiPod,
-    HRDeviceTypeSimulator
-};
-
 @interface HRDevice : NSObject
 
-+(HRDeviceType)getCurrentDeviceType;
 //获取本机IP地址局域网内
 +(NSString *)getLocalIPAddress;
 +(NSString *)deviceName;
--- HRCocoaTools/Classes/Util/HRDevice.m
+++ HRCocoaTools/Classes/Util/HRDevice.m
@@ -13,83 +13,13 @@
 #include <ifaddrs.h>
 #include <arpa/inet.h>
 
+static NSDictionary *deviceTypeStaticDic = nil;
+
 //static HRDevice     *instance = nil;
 @implementation HRDevice
 
-+(HRDeviceType)getCurrentDeviceType{
-    NSString *platForm = [self deviceModel];
-    
-    if([platForm isEqualToString:@"iPhone1,1"]){
-        return HRDeviceTypeiPhone;
-    }else if([platForm isEqualToString:@"iPhone1,2"]){
-        return HRDeviceTypeiPhone3G;
-    }else if([platForm isEqualToString:@"iPhone2,1"]){
-        return HRDeviceTypeiPhone3GS;
-    }else if([platForm hasPrefix:@"iPhone3"]){
-        //3.3 is Version iPhone that support CDMA
-        return HRDeviceTypeiPhone4;
-    }else if([platForm isEqualToString:@"iPhone4,1"]){
-        return HRDeviceTypeiPhone4S;
-    }else if([platForm isEqualToString:@"iPhone5,1"] || [platForm isEqualToString:@"iPhone5,2"]){
-        //5,2 is CDMA version
-        return HRDeviceTypeiPhone5;
-    }else if([platForm isEqualToString:@"iPhone5,3"] || [platForm isEqualToString:@"iPhone5,4"]){
-        //5,3 CDMA & WCDMA,  5,4 is TD
-        return HRDeviceTypeiPhone5c;
-    }else if([platForm hasPrefix:@"iPhone6"]){
-        //6,1 is CDMA and 6,2 is global
-        return HRDeviceTypeiPhone5S;
-    }else if([platForm isEqualToString:@"iPhone7,2"]){
-        return HRDeviceTypeiPhone6;
-    }else if([platForm isEqualToString:@"iPhone7,1"]){
-        return HRDeviceTypeiPhone6Plus;
-    }else if([platForm isEqualToString:@"iPhone8,1"]){
-        return HRDeviceTypeiPhone6s;
-    }else if([platForm isEqualToString:@"iPhone8,2"]){
-        return HRDeviceTypeiPhone6sPlus;
-    }else if([platForm isEqualToString:@"iPad1,1"]){
-        return HRDeviceTypeiPad1;
-    }else if([platForm isEqualToString:@"iPad2,1"] || [platForm isEqualToString:@"iPad2,2"] || [platForm isEqualToString:@"iPad2,3"]){
-        //2,2 is GSM and 2,1 is CDMA
-        return HRDeviceTypeiPad2;
-    }else if([platForm isEqualToString:@"iPad3,1"] || [platForm isEqualToString:@"iPad3,2"] || [platForm isEqualToString:@"iPad3,3"]){
-        return HRDeviceTypeiPad3;
-    }else if([platForm isEqualToString:@"iPad3,4"] || [platForm isEqualToString:@"iPad3,5"] || [platForm isEqualToString:@"iPad3,6"]){
-        return HRDeviceTypeiPad4;
-    }else if([platForm isEqualToString:@"iPad2,5"] || [platForm isEqualToString:@"iPad2,6"] || [platForm isEqualToString:@"iPad2,7"]){
-        return HRDeviceTypeiPadMini;
-    }else if([platForm isEqualToString:@"iPad4,4"] || [platForm isEqualToString:@"iPad4,5"] || [platForm isEqualToString:@"iPad4,6"]){
-        return HRDeviceTypeiPadMini2;
-    }else if([platForm isEqualToString:@"iPad4,7"] || [platForm isEqualToString:@"iPad4,8"] || [platForm isEqualToString:@"iPad4,9"]){
-        return HRDeviceTypeiPadMini3;
-    }else if([platForm isEqualToString:@"iPad4,1"] || [platForm isEqualToString:@"iPad4,2"] || [platForm isEqualToString:@"iPad4,3"]){
-        return HRDeviceTypeiPadAir;
-    }else if([platForm isEqualToString:@"iPad5,1"] || [platForm isEqualToString:@"iPad5,2"] || [platForm isEqualToString:@"iPad5,3"]){
-        return HRDeviceTypeiPadAir2;
-    }else if ([platForm hasPrefix:@"iPod"]){
-        return HRDeviceTypeiPod;
-    }else{
-        //Xcode 6及以后模拟器为x86_64，之前版本为iPhone simulator之类
-        if([platForm isEqualToString:@"x86_64"]){
-            return HRDeviceTypeSimulator;
-        }
-    }
-    
-    return HRDeviceTypeiPhone;
-}
-
 + (NSString *)deviceName {
-    return [[self nameEnumDic] valueForKey:@([self getCurrentDeviceType]).stringValue];
-}
-
-+(NSDictionary *)nameEnumDic{
-    NSMutableDictionary *enumDic = [NSMutableDictionary new];
-    NSArray *nameArray = @[@"iPhone",@"iPhone 3G",@"iPhone 3GS",@"iPhone 4",@"iPhone 4S",@"iPhone 5",@"iPhone 5c",@"iPhone 5s",@"iPhone 6",@"iPhone 6 Plus",@"iPhone 6s",@"iPhone 6s Plus",@"iPad 1",@"iPad 2",@"iPad 3",@"iPad 4",@"iPad Air",@"iPad Air2",@"iPad mini",@"iPad mini2",@"iPad mini3",@"iPod",@"iPhone Simulator"];
-    for(int i=0;i<22;i++){
-        [enumDic setObject:nameArray[i] forKey:@(i).stringValue];
-    }
-
-    return enumDic;
+    return [[self modelTypeMaps] valueForKey:[self deviceModel]];
 }
 
 //获取本机本地IP
@@ -141,6 +71,25 @@
     NSString *platform = [NSString stringWithCString:machine encoding:NSASCIIStringEncoding];
     free(machine);
     return platform;
+}
+
++ (NSDictionary *)modelTypeMaps {
+    NSMutableDictionary *typeDics = [NSMutableDictionary new];
+    
+    NSArray *types = [[self modelListConfig] componentsSeparatedByString:@"\n"];
+    for(NSString *typeUnit in types) {
+        NSArray *keyAndValue = [typeUnit componentsSeparatedByString:@":"];
+        NSString *modelName = keyAndValue[0];
+        NSString *modelDisplayName = keyAndValue[1];
+        
+        [typeDics setValue:modelDisplayName forKey:modelName];
+    }
+    
+    return typeDics.copy;
+}
+
++ (NSString *)modelListConfig {
+    return @"i386:iPhone Simulator\nx86_64:iPhone Simulator\niPhone1,1:iPhone\niPhone1,2:iPhone 3G\niPhone2,1:iPhone 3GS\niPhone3,1:iPhone 4\niPhone3,2:iPhone 4 GSM Rev A\niPhone3,3:iPhone 4 CDMA\niPhone4,1:iPhone 4S\niPhone5,1:iPhone 5 (GSM)\niPhone5,2 :\n iPhone 5 (GSM+CDMA)iPhone5,3:iPhone 5C (GSM)\niPhone5,4:iPhone 5C (Global)\niPhone6,1:iPhone 5S (GSM)\niPhone6,2:iPhone 5S (Global)\niPhone7,1:iPhone 6 Plus\niPhone7,2:iPhone 6\niPhone8,1:iPhone 6s\niPhone8,2:iPhone 6s Plus\niPhone8,4:iPhone SE (GSM)\niPhone9,1:iPhone 7\niPhone9,2:iPhone 7 Plus\niPhone9,3:iPhone 7\niPhone9,4:iPhone 7 Plus\niPhone10,1:iPhone 8\niPhone10,2:iPhone 8 Plus\niPhone10,3:iPhone X Global\niPhone10,4:iPhone 8\niPhone10,5:iPhone 8 Plus\niPhone10,6:iPhone X GSM\niPhone11,2:iPhone XS\niPhone11,4:iPhone XS Max\niPhone11,6:iPhone XS Max Global\niPhone11,8:iPhone XR\niPhone12,1:iPhone 11\niPhone12,3:iPhone 11 Pro\niPhone12,5:iPhone 11 Pro Max\niPod1,1:1st Gen iPod\niPod2,1:2nd Gen iPod\niPod3,1:3rd Gen iPod\niPod4,1:4th Gen iPod\niPod5,1:5th Gen iPod\niPod7,1:6th Gen iPod\niPod9,1:7th Gen iPod\niPad1,1:iPad\niPad1,2:iPad 3G\niPad2,1:2nd Gen iPad\niPad2,2:2nd Gen iPad GSM\niPad2,3:2nd Gen iPad CDMA\niPad2,4:2nd Gen iPad New Revision\niPad3,1:3rd Gen iPad\niPad3,2:3rd Gen iPad CDMA\niPad3,3:3rd Gen iPad GSM\niPad2,5:iPad mini\niPad2,6:iPad mini GSM+LTE\niPad2,7:iPad mini CDMA+LTE\niPad3,4:4th Gen iPad\niPad3,5:4th Gen iPad GSM+LTE\niPad3,6:4th Gen iPad CDMA+LTE\niPad4,1:iPad Air (WiFi)\niPad4,2:iPad Air (GSM+CDMA)\niPad4,3:1st Gen iPad Air (China)\niPad4,4:iPad mini Retina (WiFi)\niPad4,5:iPad mini Retina (GSM+CDMA)\niPad4,6:iPad mini Retina (China)\niPad4,7:iPad mini 3 (WiFi)\niPad4,8:iPad mini 3 (GSM+CDMA)\niPad4,9:iPad Mini 3 (China)\niPad5,1:iPad mini 4 (WiFi)\niPad5,2:4th Gen iPad mini (WiFi+Cellular)\niPad5,3:iPad Air 2 (WiFi)\niPad5,4:iPad Air 2 (Cellular)\niPad6,3:iPad Pro (9.7 inch, WiFi)\niPad6,4:iPad Pro (9.7 inch, WiFi+LTE)\niPad6,7:iPad Pro (12.9 inch, WiFi)\niPad6,8:iPad Pro (12.9 inch, WiFi+LTE)\niPad6,11:iPad (2017)\niPad6,12:iPad (2017)\niPad7,1:iPad Pro 2nd Gen (WiFi)\niPad7,2:iPad Pro 2nd Gen (WiFi+Cellular)\niPad7,3:iPad Pro 10.5-inch\niPad7,4:iPad Pro 10.5-inch\niPad7,5:iPad 6th Gen (WiFi)\niPad7,6:iPad 6th Gen (WiFi+Cellular)\niPad7,11:iPad 7th Gen 10.2-inch (WiFi)\niPad7,12:iPad 7th Gen 10.2-inch (WiFi+Cellular)\niPad8,1:iPad Pro 3rd Gen (11 inch, WiFi)\niPad8,2:iPad Pro 3rd Gen (11 inch, 1TB, WiFi)\niPad8,3:iPad Pro 3rd Gen (11 inch, WiFi+Cellular)\niPad8,4:iPad Pro 3rd Gen (11 inch, 1TB, WiFi+Cellular)\niPad8,5:iPad Pro 3rd Gen (12.9 inch, WiFi)\niPad8,6:iPad Pro 3rd Gen (12.9 inch, 1TB, WiFi)\niPad8,7:iPad Pro 3rd Gen (12.9 inch, WiFi+Cellular)\niPad8,8:iPad Pro 3rd Gen (12.9 inch, 1TB, WiFi+Cellular)\niPad11,1:iPad mini 5th Gen (WiFi)\niPad11,2:iPad mini 5th Gen\niPad11,3:iPad Air 3rd Gen (WiFi)\niPad11,4:iPad Air 3rd Gen\nWatch1,1:Apple Watch 38mm case\nWatch1,2:Apple Watch 42mm case\nWatch2,6:Apple Watch Series 1 38mm case\nWatch2,7:Apple Watch Series 1 42mm case\nWatch2,3:Apple Watch Series 2 38mm case\nWatch2,4:Apple Watch Series 2 42mm case\nWatch3,1:Apple Watch Series 3 38mm case (GPS+Cellular)\nWatch3,2:Apple Watch Series 3 42mm case (GPS+Cellular)\nWatch3,3:Apple Watch Series 3 38mm case (GPS)\nWatch3,4:Apple Watch Series 3 42mm case (GPS)\nWatch4,1:Apple Watch Series 4 40mm case (GPS)\nWatch4,2:Apple Watch Series 4 44mm case (GPS)\nWatch4,3:Apple Watch Series 4 40mm case (GPS+Cellular)\nWatch4,4:Apple Watch Series 4 44mm case (GPS+Cellular)\nWatch5,1:Apple Watch Series 5 40mm case (GPS)\nWatch5,2:Apple Watch Series 5 44mm case (GPS)\nWatch5,3:Apple Watch Series 5 40mm case (GPS+Cellular)\nWatch5,4:Apple Watch Series 5 44mm case (GPS+Cellular)";
 }
 
 @end
